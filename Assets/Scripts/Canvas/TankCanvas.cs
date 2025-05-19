0bcfafd1f6042a3c927b42c4b06606afc5d09554
DESKTOP-6PUUR64\Administrator <ray>
2025-05-19 15:55:39 +0800

    去掉tank1的 dotween动画

--- Assets/Scripts/Canvas/TankCanvas.cs
+++ Assets/Scripts/Canvas/TankCanvas.cs
@@ -21,32 +21,8 @@ public class TankCanvas : MonoBehaviour
             Debug.LogError("InputField or Button is not assigned in the Inspector!");
         }
         
-        if (tweenImage != null)
-        {
-            // 将tweenImage移动到(0, 0, 0)
-              PlayTween(new Vector3(500, 500, 0), 1f, 2f);
-        }
-        else
-        {
-            Debug.LogError("TweenImage未在Inspector中赋值！");
-        }
     }
-    // 调用此方法实现先移动再无限旋转
-        public void PlayTween(Vector3 targetPos, float moveDuration = 1f, float rotateDuration = 2f)
-        {
-            // 先移动
-            tweenImage.rectTransform
-                .DOMove(targetPos, moveDuration)
-                .SetEase(Ease.OutQuad)
-                .OnComplete(() =>
-                {
-                    // 移动完成后无限旋转
-                    tweenImage.rectTransform
-                        .DORotate(new Vector3(0, 0, 360), rotateDuration, RotateMode.FastBeyond360)
-                        .SetEase(Ease.Linear)
-                        .SetLoops(-1, LoopType.Restart);
-                });
-        }
+
     void OnButtonClick()
     {
         // Get and print the text from the InputField
